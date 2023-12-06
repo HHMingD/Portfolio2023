7ee2d8c8b086dc697b8ede068b9cae59a18b4e3e
Howard <howard8479@gmail.com>
2023-12-06 11:03:34 +0000

    6/12 commit

---
+++ .firebase/howard-portfolio/hosting/lib/json_parse.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+
+// import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+// import 'package:cached_network_image/cached_network_image.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:json_annotation/json_annotation.dart';
+
+import 'package:flutter/services.dart';
+
+@JsonSerializable(anyMap: true)
+class JsonStructure {
+  JsonStructure({required this.projectList, required this.smallProjectList});
+
+  final List<ProjectContent> projectList;
+  final List<SmallProjectContent> smallProjectList;
+
+  factory JsonStructure.fromJson(Map<String, dynamic> parsedJson) {
+    List<dynamic> list1 = parsedJson['Projects'];
+    List<ProjectContent> projectContent =
+        list1.map((i) => ProjectContent.fromJson(i)).toList();
+    List<dynamic> list2 = parsedJson['SmallProjects'];
+    List<SmallProjectContent> smallProjectContent =
+        list2.map((i) => SmallProjectContent.fromJson(i)).toList();
+    return JsonStructure(
+      projectList: projectContent,
+      smallProjectList: smallProjectContent,
+    );
+  }
+}
+
+@JsonSerializable(anyMap: true)
+class ProjectContent {
+  final String projectTitle;
+  final String projectTopic;
+  final String projectMyRole;
+  final String teamComposition;
+  final String projectDuration;
+  final String projectLocation;
+  final String summaryContent;
+  final String impactContent;
+  final List<ParagraphContent> paragraphContentList;
+  final List<ChallengeContent> challengeContent;
+
+  ProjectContent(
+      {required this.projectTitle,
+      required this.projectTopic,
+      required this.projectMyRole,
+      required this.summaryContent,
+      required this.teamComposition,
+      required this.projectDuration,
+      required this.projectLocation,
+      required this.impactContent,
+      required this.paragraphContentList,
+      required this.challengeContent});
+
+  factory ProjectContent.fromJson(Map<String, dynamic> parsedJson) {
+    var list1 = parsedJson['ChallengeContent'] as List;
+    List<ChallengeContent> challengeContent =
+        list1.map((i) => ChallengeContent.fromJson(i)).toList();
+    var list2 = parsedJson['ParagraphContentList'] as List;
+    List<ParagraphContent> paragraphContent =
+        list2.map((i) => ParagraphContent.fromJson(i)).toList();
+
+    return ProjectContent(
+      projectTitle: parsedJson['ProjectTitle'],
+      projectTopic: parsedJson['ProjectTopic'],
+      projectMyRole: parsedJson['ProjectMyRole'],
+      teamComposition: parsedJson['TeamComposition'],
+      projectDuration: parsedJson['ProjectDuration'],
+      projectLocation: parsedJson['ProjectLocation'],
+      summaryContent: parsedJson['SummaryContent'],
+      impactContent: parsedJson['ImpactContent'],
+      paragraphContentList: paragraphContent,
+      challengeContent: challengeContent,
+    );
+  }
+}
+
+@JsonSerializable(anyMap: true)
+class ChallengeContent {
+  final String challengeSummary;
+  final String STARTitle;
+  final String situationTitle;
+  final String situationContent;
+  final String taskTitle;
+  final String taskContent;
+  final String actionTitle;
+  final String actionContent;
+  final String resultTitle;
+  final String resultContent;
+  final List<ParagraphContent> paragraphContentList;
+
+  ChallengeContent({
+    required this.challengeSummary,
+    required this.STARTitle,
+    required this.situationContent,
+    required this.taskContent,
+    required this.actionContent,
+    required this.resultContent,
+    required this.situationTitle,
+    required this.taskTitle,
+    required this.actionTitle,
+    required this.resultTitle,
+    required this.paragraphContentList,
+  });
+
+  factory ChallengeContent.fromJson(Map<String, dynamic> parsedJson) {
+    var list = parsedJson['ParagraphContentList'] as List;
+    List<ParagraphContent> content =
+        list.map((i) => ParagraphContent.fromJson(i)).toList();
+    return ChallengeContent(
+      challengeSummary: parsedJson['ChallengeSummary'],
+      STARTitle: parsedJson['STARTitle'],
+      situationTitle: parsedJson['SituationTitle'],
+      situationContent: parsedJson['SituationContent'],
+      taskTitle: parsedJson['TaskTitle'],
+      taskContent: parsedJson['TaskContent'],
+      actionTitle: parsedJson['ActionTitle'],
+      actionContent: parsedJson['ActionContent'],
+      resultTitle: parsedJson['ResultTitle'],
+      resultContent: parsedJson['ResultContent'],
+      paragraphContentList: content,
+    );
+  }
+}
+
+@JsonSerializable(anyMap: true)
+class SmallProjectContent {
+  final String projectTitle;
+  final String projectTopic;
+  final String projectMyRole;
+  final String projectDuration;
+  final String challengeSummary;
+  final List<ParagraphContent> paragraphContentList;
+
+  SmallProjectContent({
+    required this.projectTitle,
+    required this.projectTopic,
+    required this.projectMyRole,
+    required this.projectDuration,
+    required this.challengeSummary,
+    required this.paragraphContentList,
+  });
+
+  factory SmallProjectContent.fromJson(Map<String, dynamic> parsedJson) {
+    var list = parsedJson['ParagraphContentList'] as List;
+    List<ParagraphContent> content =
+        list.map((i) => ParagraphContent.fromJson(i)).toList();
+    return SmallProjectContent(
+      projectTitle: parsedJson['ProjectTitle'],
+      projectTopic: parsedJson['ProjectTopic'],
+      projectMyRole: parsedJson['ProjectMyRole'],
+      projectDuration: parsedJson['ProjectDuration'],
+      challengeSummary: parsedJson['ChallengeSummary'],
+      paragraphContentList: content,
+    );
+  }
+}
+
+@JsonSerializable(anyMap: true)
+class ParagraphContent {
+  final String titleText;
+  final String subtitleText;
+  final String imageLocation;
+  final String contentText;
+  final String layout;
+  final LinkAddress link;
+
+  ParagraphContent(
+      {required this.titleText,
+      required this.subtitleText,
+      required this.imageLocation,
+      required this.contentText,
+      required this.layout,
+      required this.link});
+
+  factory ParagraphContent.fromJson(Map<String, dynamic> parsedJson) {
+    bool linkActive = parsedJson['Link']["LinkActive"];
+    int linkAdressPage = parsedJson['Link']["Page"];
+    int linkAdressProject = parsedJson['Link']["Project"];
+    int linkAdressChallenge = parsedJson['Link']["Challenge"];
+    LinkAddress linkAddress = LinkAddress(
+        active: linkActive,
+        page: linkAdressPage,
+        project: linkAdressProject,
+        challenge: linkAdressChallenge);
+
+    return ParagraphContent(
+      titleText: parsedJson['TitleText'],
+      subtitleText: parsedJson['SubtitleText'],
+      imageLocation: parsedJson['ImageLocation'],
+      contentText: parsedJson['ContentText'],
+      layout: parsedJson['Layout'],
+      link: linkAddress,
+    );
+  }
+}
+
+@JsonSerializable(anyMap: true)
+class LinkAddress {
+  final bool active;
+  final int page;
+  final int project;
+  final int challenge;
+
+  LinkAddress({
+    required this.active,
+    required this.page,
+    required this.project,
+    required this.challenge,
+  });
+
+  factory LinkAddress.fromJson(Map<String, dynamic> parsedJson) {
+    return LinkAddress(
+      active: parsedJson['LinkActive'],
+      page: parsedJson['Page'],
+      project: parsedJson['Project'],
+      challenge: parsedJson['Challenge'],
+    );
+  }
+}
---
+++ .firebase/howard-portfolio/hosting/lib/network.dart
@@ -0,0 +1,131 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'app_theme.dart';
+import 'json_parse.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+final contentRef = FirebaseStorage.instance.ref('content.json');
+
+Future<List<ProjectContent>> readProjectJsonOnline() async {
+  final String url = await contentRef.getDownloadURL();
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    var parsedJson = await json.decode(response.body);
+    List<ProjectContent> projectList = List<ProjectContent>.from(parsedJson
+        .map<ProjectContent>((dynamic i) => ProjectContent.fromJson(i))
+        .toList());
+    return projectList;
+  } else {
+    throw Exception('Failed to load project');
+  }
+}
+
+Future<JsonStructure> readJsonStructureOnline() async {
+  final String url = await contentRef.getDownloadURL();
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    var parsedJson = await json.decode(response.body);
+    JsonStructure jsonStructure = JsonStructure.fromJson(parsedJson);
+    return JsonStructure(
+      projectList: jsonStructure.projectList,
+      smallProjectList: jsonStructure.smallProjectList,
+    );
+  } else {
+    throw Exception('Failed to load project');
+  }
+}
+
+late OverlayEntry overlayEntry;
+
+class FutureImageBuilder extends StatelessWidget {
+  const FutureImageBuilder(this.imageLink, {super.key});
+
+  final String imageLink;
+
+  Future<String> fetchImageUrl(String value) async {
+    return FirebaseStorage.instance.ref(value).getDownloadURL();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<String>(
+        future: fetchImageUrl(
+          imageLink,
+        ),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: FadeInImage.assetNetwork(
+                fadeOutDuration: const Duration(milliseconds: 100),
+                fadeInDuration: const Duration(milliseconds: 100),
+                placeholder: 'imageplaceholder.png',
+                image: snapshot.data!,
+                fit: BoxFit.cover,
+              ),
+            );
+          } else {
+            return const SizedBox();
+          }
+        });
+  }
+}
+
+class ImageWithOverlay extends StatefulWidget {
+  const ImageWithOverlay(this.imageDownloadURL, {super.key});
+
+  final String imageDownloadURL;
+
+  @override
+  State<ImageWithOverlay> createState() => _ImageWithOverlayState();
+}
+
+class _ImageWithOverlayState extends State<ImageWithOverlay> {
+  void imageOverlay(String location) {
+    overlayEntry = myOverlayEntry(location);
+    Overlay.of(context).insert(overlayEntry);
+  }
+
+  OverlayEntry myOverlayEntry(String location) {
+    return OverlayEntry(
+        maintainState: true,
+        builder: (context) {
+          return GestureDetector(
+            onTap: () {
+              overlayEntry?.remove();
+            },
+            child: Container(
+              padding: const EdgeInsets.all(32),
+              decoration: const BoxDecoration(color: Color(0x66000000)),
+              child: Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.85,
+                  child: FutureImageBuilder(
+                    location,
+                  ),
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {
+          imageOverlay(widget.imageDownloadURL);
+        },
+        child: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: const BoxDecoration(
+                color: Apptheme.imageContrast,
+                borderRadius: BorderRadius.all(Radius.circular(8))),
+            child: FutureImageBuilder(widget.imageDownloadURL)));
+  }
+}
